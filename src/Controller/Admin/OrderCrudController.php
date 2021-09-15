9e8451d4f6cbb9d1ab2cbd1c093d22ceaeb6c54e
Abishan13 <arul.abishanpro@gmail.com>
2021-09-15 15:20:08 +0200

    Setup Heroku

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -61,11 +61,11 @@ class OrderCrudController extends AbstractCrudController
 
         $url = $this->crudUrlGenerator->build()
             ->setController(OrderCrudController::class)
-            ->setAction('detail')
+            ->setAction(Action::INDEX)
             ->generateUrl();
 
             $mail = new Mail();
-            $content = "Bonjour ".$order->getUser()->getFirstname()."<br/>Bonjour ".$order->getUser()->getFirstname()."<br> Votre commande dont la référence est : <b>".$order->getReference()."<b/> est en cours de préparation.";
+            $content = "<br/>Bonjour ".$order->getUser()->getFirstname()."<br><b> Votre commande dont la référence est : ".$order->getReference()." est en cours de préparation.<b/>";
             $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstname(), 'Votre commande Karasuno est en cours de préparation.', $content);
 
         return $this->redirect($url);
@@ -81,15 +81,15 @@ class OrderCrudController extends AbstractCrudController
 
         $url = $this->crudUrlGenerator->build()
             ->setController(OrderCrudController::class)
-            ->setAction('detail')
+            ->setAction('index')
             ->generateUrl();
 
 
             
             
     $mail = new Mail();
-    $content = "Bonjour ".$order->getUser()->getFirstname()."<br/>Votre commande est en cours de livraison<br><br/>";
-    $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstname(), 'Votre commande est en cours de livraison.', $content);
+    $content = "<br/>Bonjour ".$order->getUser()->getFirstname()."<br><b> Votre commande dont la référence est : ".$order->getReference()." est en cours de livraison.<b/>";
+    $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstname(), 'Votre commande Karasuno est en cours de livraison.', $content);
 
 
        return $this->redirect($url);
